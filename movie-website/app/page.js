f1b9f9ddc48c9840eebd890ef95c56118505ff94
simon asefa <yohanness803@gmail.com>
2023-10-06 00:32:23 +0200

    add files

--- movie-website/app/page.js
+++ movie-website/app/page.js
@@ -4,13 +4,13 @@ import NavBar from './components/nav-bar';
 import movies from "./data/movies.json"
 import { Movie } from './components/movie';
 export default function HOME(params) {
-  const movie = movies['0'];
+  // const movie = movies['0'];
   const movieList = movies.map((movie)=>{
     return <Movie movie={movie} key={movie.Title}/>
   })
     return <div>
         <NavBar/>
-      <div style={{display: "flex"}}>  {movieList}</div>
+      <div style={{display: "flex" }}>  {movieList}</div>
         </div>
 }
 
